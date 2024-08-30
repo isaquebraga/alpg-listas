58983ba9175584cbecc19546e7b5a5d89cc49194
isaque <isaquesouzabraga@gmail.com>
2024-08-29 22:00:04 -0300

    realização da questão 04

--- questao-04.py
+++ questao-04.py
@@ -1,10 +1,12 @@
 """
     Questão 4:
     
-    Três jesuítas e três canibais precisam atravessar um rio; para tal, dispõem de um barco com capacidade para duas pessoas. Por medida de segurança, não se deve permitir que em alguma margem a quantidade de jesuítas seja inferior à de canibais. Qual a solução para efetuar a travessia com segurança? Elabore um algoritmo mostrando a resposta, indicando as ações que concretizam a solução deste problema, lembrando que, sempre tem que ter uma pessoa para conduzir o barco.
+    Três jesuítas e três canibais precisam atravessar um rio; para tal, dispõem de um barco com capacidade para duas pessoas. Por medida de segurança, não se deve permitir que em alguma margem a quantidade de jesuítas seja inferior à de canibais. 
+    
+    Qual a solução para efetuar a travessia com segurança? Elabore um algoritmo mostrando a resposta, indicando as ações que concretizam a solução deste problema, lembrando que, sempre tem que ter uma pessoa para conduzir o barco.
 """
 
-print("Movimentos para atravessar o rio com 3 jesuítas e 3 canibais:")
+print("Movimentos para atravessar o rio com 3 jesuítas e 3 canibais:\n")
 
 print("1. Enviar 2 canibais da margem A para a margem B.")
 print("2. Voltar 1 canibal da margem B para a margem A.")
